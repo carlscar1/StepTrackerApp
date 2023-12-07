719ba57f23c233b63cc7098c1b5acf46b2218567
carlscar1 <79236208+carlscar1@users.noreply.github.com>
2023-12-06 23:27:13 -0500

    Added in the healthkit features, so it reads the number of steps from the healthapp

--- StepTrackerApp/MainViewController.swift
+++ StepTrackerApp/MainViewController.swift
@@ -6,28 +6,79 @@
 //
 
 import UIKit
+import SwiftUI
+import HealthKit
 
 class MainViewController: UIViewController {
 
+    private let healthStore = HKHealthStore()
+    
+    @IBOutlet weak var stepsLabel: UILabel!
     @IBOutlet weak var loginLabel: UILabel!
+    
     var userEmail : String?
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            if let email = self.userEmail {
+        super.viewDidLoad()
+        if let email = self.userEmail {
                 self.loginLabel.text = email
-            }
+        }
+        
+        authorizeHealthKit() // This function provides to authorize the HealthKit.
+        
+        gettingStepCount() { stepCount in // This function provides to get the step count of the users.
+                    DispatchQueue.main.async {
+                        print("Step count is:", Int(stepCount)) // Get the step count.
+                        self.stepsLabel.text = "You have walked \(Int(stepCount)) steps!"
+                    }
+        }
     }
     
+        private func authorizeHealthKit() {
+            let healthKitTypes: Set = [ HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)! ] // We want to access the step count.
+            healthStore.requestAuthorization(toShare: nil, read: healthKitTypes) { (success, error) in  // We will check the authorization.
+                if success {} // Authorization is successful.
+            }
+        }
+    
+        private func gettingStepCount(completion: @escaping (Double) -> Void) {
+            var mSample = 0.0
+            let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+            let sampleQuery = HKSampleQuery.init(sampleType: type,
+                                                 predicate: get24hPredicate(),
+                                                 limit: HKObjectQueryNoLimit,
+                                                 sortDescriptors: nil,
+                                                 resultsHandler: { (query, results, error) in
+                
+                guard let samples = results as? [HKQuantitySample] else {
+                    print(error!)
+                    return
+                }
+                for sample in samples {
+                    mSample = mSample + sample.quantity.doubleValue(for: HKUnit(from: "count"))
+                    //print("Step count : \(mSample)")
+                    //self.stepsLabel.text = "You have walked \(Int(mSample)) steps!"
+                }
+                print("Step count : \(mSample)")
+                //self.stepsLabel.text = "You have walked \(Int(mSample)) steps!"
+                DispatchQueue.main.async {
+                    print("Step count is:", Int(mSample)) // Get the step count.
+                    self.stepsLabel.text = "You have walked \(Int(mSample)) steps!"
+                }
+            })
+            //self.stepsLabel.text = "You have walked \(Int(mSample)) steps!"
+            self.healthStore .execute(sampleQuery)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        }
+    
+    private func get24hPredicate() ->  NSPredicate{
+            let today = Date()
+            let startDate = Calendar.current.date(byAdding: .hour, value: -24, to: today)
+            let predicate = HKQuery.predicateForSamples(withStart: startDate,end: today,options: [])
+            return predicate
     }
-    */
 
+    
 }
+
+
